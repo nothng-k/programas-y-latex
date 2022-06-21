767d460295fbd91fe1ed1214fdd737ca15665e43
nothng-k <manuel.gr@hotmail.es>
2022-06-21 12:21:57 +0200

    ya bien
    la cague, pero ahora bien

--- prueba.R
+++ prueba.R
@@ -3,7 +3,7 @@ graficador<-function(a,b,l,f){
   x <- seq(a, b, length.out = l)
   y <- x
   z <- outer(x, y, f)
-
+  z[z<0]=0
   op <- par(bg = "white")
   persp(x, y, z, theta = 30, phi = 30, expand = 0.5, col = "lightblue")
   persp(x, y, z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
@@ -16,13 +16,13 @@ price_function<-function(p_1,p_2,k=1){
   return(exp((p_2-p_1)*k))
 }
 
-price_function<-function(p_1,p_2,k=400){
+price_function<-function(p_1,p_2,k=200){
   return(1-(
     exp(p_1/k)/
-     (exp(p_1/k)+exp(p_2/k))
+      (exp(p_1/k)+exp(p_2/k))
   )
-      
-     )
+  
+  )
 }
 
 
@@ -32,10 +32,10 @@ dicotomic_function<-function(c_1,c_2,n_1,n_2,N){
   return(
     (
       n_1+c_1*(N-n_1-n_2)
-      )/(
-        (n_1+n_2)*((c_1+c_2)==0)+N*((c_1+c_2)>0)
-        )
+    )/(
+      (n_1+n_2)*((c_1+c_2)==0)+N*((c_1+c_2)>0)
     )
+  )
 }
 
 dicotomic_function<-function(c_1,c_2,n_1,n_2,N){
@@ -50,7 +50,7 @@ dicotomic_function<-function(c_1,c_2,n_1,n_2,N){
 
 dicotomic_function1<-function(c_1,c_2,n_1,n_2,N){
   return(
-      c_1 * c_2 * 0.6 +
+    c_1 * c_2 * 0.6 +
       c_1 * (1 - c_2) * 0.75 +
       (1 -c_1) * ( 1 - c_2 ) * 0.65+
       c_1 * (1 - c_2) * 0.8
@@ -59,37 +59,45 @@ dicotomic_function1<-function(c_1,c_2,n_1,n_2,N){
 dicotomic_function2<-function(c_1,c_2,n_1,n_2,N){
   return(
     c_1 * c_2 * 0.6 +
-    c_1 * (1 - c_2) * 0.75 +
-    (1 -c_1) * ( 1 - c_2 ) * 0.65+
-    c_1 * (1 - c_2) * 0.8
+      c_1 * (1 - c_2) * 0.75 +
+      (1 -c_1) * ( 1 - c_2 ) * 0.65+
+      c_1 * (1 - c_2) * 0.8
   )
 }
 
 price_control<-function(p,k=400,m=1000){
   return(0+
-    (p<m)*exp(-p/k)
+           (p<m)*((p/k)^2+m)
   )
 }
+price_control<-function(p,k=400,m=1000){
+  return(0+
+           (p<m)*exp(-p/k)*((p/-m)^2)
+  )
+}
+
 
 m_ps<-function(p_1,p_2,c_1=0,c_2=0,n_1=100000000,n_2=50000000,N=500000000,k=0.03){
   return(
     2000*dicotomic_function(c_1,c_2,n_1,n_2,N)*price_function(p_1,p_2,k)*price_control(p_1)
   )
 }
-graficador(0,2,60,"m_ps")
+graficador(1,10,60,"m_ps")
 
 V_ps<-function(p_1,p_2,c_1=0,c_2=0,n_1=100000000,n_2=50000000,N=500000000,k=400){
   m_ps(p_1,p_2,c_1=c_1,c_2=c_2,k=k,n_1 = n_1,n_2=n_2,N=N)*(p_1-300)
 }
-graficador(300,1000,60,"V_ps")
-k=400
+graficador(1,1000,60,"V_ps")
+k=1000
 p=200
-numeros=numeric(k-p)
-for( i in p:k){
-  numero=c(0,0)
-  for( j in p:k){
-    if (numero[1]<V_ps(j,i)){
-      numero=c(V_ps(j,i),j)
+numeros=numeric(k)
+a=0
+numero =0
+for( i in 1:k){
+  for( j in 1:k){
+    if (a < V_ps(j,i)){
+      numero=j
+      a=V_ps(j,i)
     }
   }
   numeros[i]=numero
@@ -97,3 +105,4 @@ for( i in p:k){
 numeros
 plot(numeros)
 abline(1,1, col = 2)
+
