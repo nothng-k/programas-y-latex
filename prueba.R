acccd0d0952c22693c4fda82f75bfa54867d3d03
nothng-k <manuel.gr@hotmail.es>
2022-06-21 11:53:03 +0200

    prueba

--- prueba.R
+++ prueba.R
@@ -3,7 +3,7 @@ graficador<-function(a,b,l,f){
   x <- seq(a, b, length.out = l)
   y <- x
   z <- outer(x, y, f)
-  z[z<0]=0
+
   op <- par(bg = "white")
   persp(x, y, z, theta = 30, phi = 30, expand = 0.5, col = "lightblue")
   persp(x, y, z, theta = 30, phi = 30, expand = 0.5, col = "lightblue",
@@ -16,7 +16,7 @@ price_function<-function(p_1,p_2,k=1){
   return(exp((p_2-p_1)*k))
 }
 
-price_function<-function(p_1,p_2,k=200){
+price_function<-function(p_1,p_2,k=400){
   return(1-(
     exp(p_1/k)/
      (exp(p_1/k)+exp(p_2/k))
@@ -67,37 +67,38 @@ dicotomic_function2<-function(c_1,c_2,n_1,n_2,N){
 
 price_control<-function(p,k=400,m=1000){
   return(0+
-    (p<m)*((p/k)^2+m)
+    (p<m)*exp(-p/k)
   )
 }
+<<<<<<< HEAD
 price_control<-function(p,k=400,m=1000){
   return(0+
            (p<m)*exp(-p/k)*((p/-m)^2)
   )
 }
-
+    
+=======
+>>>>>>> parent of 4e94971 (cambios)
 
 m_ps<-function(p_1,p_2,c_1=0,c_2=0,n_1=100000000,n_2=50000000,N=500000000,k=0.03){
   return(
     2000*dicotomic_function(c_1,c_2,n_1,n_2,N)*price_function(p_1,p_2,k)*price_control(p_1)
   )
 }
-graficador(1,10,60,"m_ps")
+graficador(0,2,60,"m_ps")
 
 V_ps<-function(p_1,p_2,c_1=0,c_2=0,n_1=100000000,n_2=50000000,N=500000000,k=400){
   m_ps(p_1,p_2,c_1=c_1,c_2=c_2,k=k,n_1 = n_1,n_2=n_2,N=N)*(p_1-300)
 }
-graficador(1,1000,60,"V_ps")
-k=1000
+graficador(300,1000,60,"V_ps")
+k=400
 p=200
-numeros=numeric(k)
-a=0
-numero =0
-for( i in 1:k){
-  for( j in 1:k){
-    if (a < V_ps(j,i)){
-      numero=j
-      a=V_ps(j,i)
+numeros=numeric(k-p)
+for( i in p:k){
+  numero=c(0,0)
+  for( j in p:k){
+    if (numero[1]<V_ps(j,i)){
+      numero=c(V_ps(j,i),j)
     }
   }
   numeros[i]=numero
@@ -105,4 +106,3 @@ for( i in 1:k){
 numeros
 plot(numeros)
 abline(1,1, col = 2)
-
