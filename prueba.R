d33315fefbd9c815198c37d8bbcce586a3b11dd6
nothng-k <manuel.gr@hotmail.es>
2022-06-22 10:04:22 +0200

    Cambios realizados durante la tutoría 22/6
    Cambiamos un poco las funciones y creamos la que acontecen a xbox.
    
    También Jorge creó una función para obtener el equilibrio de Nash, yo cree una versión de hacendado

--- prueba.R
+++ prueba.R
@@ -59,20 +59,20 @@ dicotomic_function1<-function(c_1,c_2,n_1,n_2,N){
 dicotomic_function2<-function(c_1,c_2,n_1,n_2,N){
   return(
     c_1 * c_2 * 0.6 +
-      c_1 * (1 - c_2) * 0.75 +
-      (1 -c_1) * ( 1 - c_2 ) * 0.65+
+      c_1 * (1 - c_2) * 0.65 +
+      (1 -c_1) * ( 1 - c_2 ) * 0.75+
       c_1 * (1 - c_2) * 0.8
   )
 }
 
 price_control<-function(p,k=400,m=1000){
   return(0+
-           (p<m)*((p-m)^2)
+           (p<m)*(((p-m)/m)^2)
   )
 }
 price_control<-function(p,k=400,m=1000){
   return(0+
-           (p<m)*exp(-p/k)*((p-m)^2)
+          (p<m)*exp(-p/k)*(((p-m)/m)^2)
   )
 }
 
@@ -87,9 +87,14 @@ graficador(1,10,60,"m_ps")
 V_ps<-function(p_1,p_2,c_1=0,c_2=0,n_1=100000000,n_2=50000000,N=500000000,k=400){
   m_ps(p_1,p_2,c_1=c_1,c_2=c_2,k=k,n_1 = n_1,n_2=n_2,N=N)*(p_1-100)
 }
+
+V_xb<-function(p_1,p_2,c_1=0,c_2=0,n_1=100000000,n_2=50000000,N=500000000,k=300){
+  m_ps(p_2,p_1,c_1=c_1,c_2=c_2,k=k,n_1 = n_1,n_2=n_2,N=N)*(p_2-50)
+}
+
 graficador(1,1000,60,"V_ps")
 k=1000
-P=10000
+P=1000
 numeros=numeric(P)
 a=0
 numero =0
@@ -104,5 +109,21 @@ for( i in 1:P){
 }
 numeros
 plot(numeros)
+c=10000
+ps=numeric(c)
+xb=numeric(c)
+xb[1]=0
+for(i in 1:1000){
+  ps[i+1]=numeros[xb[i]]
+  xb[i+1]=numeros[ps[i+1]]
+}
 
+x0 = 0
+y0 = 0
+c = 100
+z = matrix(0,nrow=2,ncol=c+1); z[,1] <- c(x0,y0)
+for(i in 1:c){
+  z[1,i+1] <- optim(z[1,i],function(x){-V_ps(x,z[2,i])})$par
+  z[2,i+1] <- optim(z[2,i],function(y){-V_xb(z[1,i+1],y)})$par
+}
 
